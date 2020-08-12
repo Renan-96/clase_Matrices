1cfcd779b37956358b486d599ddf22340c64049f
Renan-96 <renan.salcedo@virginiogomez.cl>
2020-08-12 18:10:23 -0400

    se agregaro nuevo static void
    se creo static void Resta

--- Matrices/Ejercicio_2/Program.cs
+++ Matrices/Ejercicio_2/Program.cs
@@ -14,10 +14,13 @@ namespace Ejercicio_2
 
             int numero1 = 10;
             int numero2 = 20;
-
+            
             Suma(numero1, numero2);//se pueden elegir las variables o se pueden designar valores)
             Suma(32, 12);
             Suma(123, -12);
+            Resta(numero1,numero2);
+            Resta(102, 93);
+            Resta(21, -12);
             
             Console.ReadKey();
         }
@@ -25,12 +28,23 @@ namespace Ejercicio_2
         {
             if (n1<0 || n2<0)
             {
-                Console.WriteLine("No se pueden sumar numeros negativos");
+                Console.WriteLine("No se pueden restar numeros negativos");
             }
             else
             {
                 Console.WriteLine(n1 + n2);
             }            
+        }
+        static void Resta(int n1, int n2)
+        {
+            if (n1 < 0 || n2 < 0)
+            {
+                Console.WriteLine("No se pueden sumar numeros negativos");
+            }
+            else
+            {
+                Console.WriteLine(n1 - n2);
+            }
         }
 
     }
